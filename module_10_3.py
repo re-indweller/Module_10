d6e81445a96a727adbfed01ec0580c38c6cd91a1
re-indweller <asf.ayrat@gmail.com>
2024-11-03 20:17:52 +0300

    Add files via upload

---
+++ module_10_3.py
@@ -0,0 +1,45 @@
+# Домашнее задание по теме "Блокировки и обработка ошибок"
+# Задача "Банковские операции"
+
+import threading
+import random
+import time
+
+class Bank:
+    def __init__(self):
+        self.balance = 0
+        self.lock = threading.Lock()
+
+    def deposit(self):
+        for i in range(100):
+            if self.balance >= 500 and self.lock.locked():
+                self.lock.release()
+            plenishment = random.randint(50, 500)
+            self.balance += plenishment
+            print(f'Пополнение: {plenishment}. Баланс: {self.balance}')
+        time.sleep(0.001)
+
+    def take(self):
+        for i in range(100):
+            withdrawing = random.randint(50, 500)
+            print(f'Запрос на {withdrawing}')
+            if self.balance >= withdrawing:
+                self.balance -= withdrawing
+                print(f'Снятие: {withdrawing}. Баланс: {self.balance}')
+            else:
+                print(f'Запрос отклонён, недостаточно средств')
+                self.lock.acquire()
+            time.sleep(0.001)
+
+bk = Bank()
+
+# Т.к. методы принимают self, в потоки нужно передать сам объект класса Bank
+th1 = threading.Thread(target=Bank.deposit, args=(bk,))
+th2 = threading.Thread(target=Bank.take, args=(bk,))
+
+th1.start()
+th2.start()
+th1.join()
+th2.join()
+
+print(f'Итоговый баланс: {bk.balance}')
